d47fe8b6877154086b710b35670ead16c9c20ecf
Stévan Corre <stevancorre@protonmail.com>
2022-10-29 00:46:49 +0200

    [scraper/providers/culturefactory] implement

--- scraper/src/main/java/com/stevancorre/cda/providers/CulturefactoryProvider.java
+++ scraper/src/main/java/com/stevancorre/cda/providers/CulturefactoryProvider.java
@@ -1,24 +1,41 @@
 package com.stevancorre.cda.providers;
 
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.*;
 import com.stevancorre.cda.providers.abstraction.Provider;
 import com.stevancorre.cda.providers.abstraction.SearchResult;
 
+import java.io.IOException;
 import java.util.List;
 
-public class CulturefactoryProvider extends Provider {
+import static com.stevancorre.cda.utils.Scraping.scrapPrice;
+
+public final class CulturefactoryProvider extends Provider<HtmlArticle> {
     @Override
-    protected String getQueryUrl(String query) {
-        return null;
+    protected String getQueryUrl(final String query) {
+        return String.format("https://culturefactory.fr/recherche?controller=search&s=%s", query.replace(" ", "+"));
     }
 
     @Override
-    protected List scrapEntries(HtmlPage page) {
-        return null;
+    protected List<HtmlArticle> scrapEntries(final HtmlPage page) {
+        return page.getByXPath("//article[@class='js-product-miniature item_in']");
     }
 
     @Override
-    protected SearchResult scrapNext(Object source) {
-        return null;
+    protected SearchResult scrapNext(final HtmlArticle source) throws IOException {
+        final HtmlAnchor anchor = source.getFirstByXPath(".//a");
+        final String url = anchor.getHrefAttribute();
+        final HtmlPage page = client.getPage(url);
+
+        final HtmlHeading1 title = page.getFirstByXPath("//h1[@itemprop='name']");
+        final HtmlSpan price = page.getFirstByXPath("//span[@itemprop='price']");
+        final HtmlDivision description = page.getFirstByXPath("//div[@itemprop='description']");
+
+        return new SearchResult(
+                url,
+                this,
+                "https://via.placeholder.com/500",
+                title.getTextContent(),
+                description == null ? "No description" : description.getTextContent(),
+                scrapPrice(price));
     }
 }
