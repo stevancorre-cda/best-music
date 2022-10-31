5aa45e4b36df35dbc0d8aba0baad0b384627d478
Stévan Corre <stevancorre@protonmail.com>
2022-10-31 10:00:59 +0100

    [scraper/providers/fnac] implement

--- scraper/src/main/java/com/stevancorre/cda/providers/FnacProvider.java
+++ scraper/src/main/java/com/stevancorre/cda/providers/FnacProvider.java
@@ -1,25 +1,44 @@
 package com.stevancorre.cda.providers;
 
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.*;
 import com.stevancorre.cda.providers.abstraction.Provider;
 import com.stevancorre.cda.providers.abstraction.SearchResult;
 
+import java.io.IOException;
 import java.util.List;
+
+import static com.stevancorre.cda.utils.Scraping.scrapDescription;
+import static com.stevancorre.cda.utils.Scraping.scrapPrice;
 
 public final class FnacProvider extends Provider {
     @Override
     protected String getQueryUrl(final String query) {
-        return null;
+        return String.format(
+                "https://www.fnac.com/SearchResult/ResultList.aspx?SCat=Musique%%211&Search=%s",
+                query.replace(" ", "+"));
     }
 
     @Override
     protected List<HtmlElement> scrapEntries(final HtmlPage page) {
-        return null;
+        return page.getByXPath("//div[@class='clearfix Article-item js-Search-hashLinkId']");
     }
 
     @Override
-    protected SearchResult scrapNext(final HtmlElement source) {
-        return null;
+    protected SearchResult scrapNext(final HtmlElement source) throws IOException {
+        final HtmlAnchor anchor = source.getFirstByXPath(".//a");
+        final String url = anchor.getHrefAttribute();
+        final HtmlPage page = client.getPage(url);
+
+        final HtmlHeading1 title = page.getFirstByXPath("//h1[@class='f-productHeader-Title']");
+        final HtmlSpan price = page.getFirstByXPath("//span[@class='f-faPriceBox__price userPrice js-ProductBuy-standardCheckable checked']");
+        final HtmlDivision description = page.getFirstByXPath("//div[@class='f-productDesc__raw']");
+
+        return new SearchResult(
+                url,
+                this,
+                "https://via.placeholder.com/500",
+                title.getTextContent(),
+                scrapDescription(description),
+                scrapPrice(price));
     }
 }
