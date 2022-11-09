8402e443461cb330c73760dcba3f6a1864339764
Stévan Corre <stevancorre@protonmail.com>
2022-11-09 11:54:47 +0100

    [scraper/module-info] fix javafx graphics

--- scraper/src/main/java/module-info.java
+++ scraper/src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module com.stevancorre.cda.scraper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.sql;
     requires sib.api.v3.sdk;
     requires htmlunit;
-    requires com.google.common;
     requires org.mybatis;
+    requires com.google.common;
 
     opens com.stevancorre.cda.scraper.controllers to javafx.fxml;
     exports com.stevancorre.cda.scraper;
