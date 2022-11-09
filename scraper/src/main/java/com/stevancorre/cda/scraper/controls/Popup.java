ba3128443e7cc7b396ce301578f8c8e059f2a61d
Stévan Corre <stevancorre@protonmail.com>
2022-11-09 22:06:17 +0100

    [scraper/controls/popup] add controller settings func

--- scraper/src/main/java/com/stevancorre/cda/scraper/controls/Popup.java
+++ scraper/src/main/java/com/stevancorre/cda/scraper/controls/Popup.java
@@ -1,19 +1,31 @@
 package com.stevancorre.cda.scraper.controls;
 
 import com.stevancorre.cda.scraper.Main;
+import com.stevancorre.cda.scraper.controllers.MainController;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.function.Consumer;
 
 public final class Popup extends Stage {
     public Popup(final String title, final String fxml, final int width, final int height) throws IOException {
+        this(title, fxml, width, height, c -> {
+        });
+    }
+
+    public <T> Popup(final String title, final String fxml, final int width, final int height, final Consumer<T> controllerInit) throws IOException {
         super();
 
         final FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml));
-        final Scene scene = new Scene(fxmlLoader.load(), width, height);
+        final Parent root = fxmlLoader.load();
+        final T controller = fxmlLoader.getController();
+        controllerInit.accept(controller);
+
+        final Scene scene = new Scene(root, width, height);
 
         initModality(Modality.APPLICATION_MODAL);
         setTitle(title);
